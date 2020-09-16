ae80f128c8c9c1aa46822ee1f1416f27a60a9d44
chigirits <chigiri@haq.jp>
2020-09-16 19:13:26 +0900

    v1.1.0

--- Assets/Chigiri/MeshHoleShrinker/Scripts/MeshHoleShrinker.cs
+++ Assets/Chigiri/MeshHoleShrinker/Scripts/MeshHoleShrinker.cs
@@ -5,10 +5,13 @@ namespace Chigiri.MeshHoleShrinker
 
     public class MeshHoleShrinker : MonoBehaviour
     {
-        public SkinnedMeshRenderer targetMesh;
+        public SkinnedMeshRenderer targetRenderer;
+        public Mesh sourceMesh;
         public string newName = "shrink_hole";
         public float scale = 0.5f;
+        public Vector3 offset = Vector3.zero;
         public float epsilon = 1e-3f;
+        public bool useMeshCollider = false;
         public GameObject meshSnapshotPrefab;
     }
 
